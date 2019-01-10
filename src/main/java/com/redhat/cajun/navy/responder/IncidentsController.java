d61104eee3d2bdb1f85d500f7e080d1179cf0562
Dillon Winchell <dwinchel@redhat.com>
2019-01-10 11:49:55 -0500

    Fixed copy paste error

--- src/main/java/com/redhat/cajun/navy/responder/IncidentsController.java
+++ src/main/java/com/redhat/cajun/navy/responder/IncidentsController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/responders")
+@RequestMapping("/incidents")
 public class IncidentsController {
 
     @RequestMapping("/map")
