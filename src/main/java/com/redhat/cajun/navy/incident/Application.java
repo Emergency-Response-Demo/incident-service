84e5e3914df2b62d19d58051168a69326c5ebb43
Bernard Tison <bernard.tison@gmail.com>
2019-05-02 08:28:08 +0200

    Moved swagger configuration to separate Spring configuration bean

--- src/main/java/com/redhat/cajun/navy/incident/Application.java
+++ src/main/java/com/redhat/cajun/navy/incident/Application.java
@@ -2,25 +2,9 @@ package com.redhat.cajun.navy.incident;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class Application {
-
-    @Bean
-    public Docket swagger() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.redhat.cajun.navy.incident"))
-                .paths(PathSelectors.any())
-                .build();
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/redhat/cajun/navy/incident/SwaggerConfiguration.java
@@ -0,0 +1,26 @@
+package com.redhat.cajun.navy.incident;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile("!test")
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.redhat.cajun.navy.incident"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
